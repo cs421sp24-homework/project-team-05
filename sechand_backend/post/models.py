073943733ae4f3ce4266f3c33024da8a59c1c961
Todd Tao <jtao26@jhu.edu>
2024-02-23 14:54:10 -0500

    add DELETE API

--- sechand_backend/post/models.py
+++ sechand_backend/post/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
 class Item(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128)
--- sechand_backend/post/urls.py
+++ sechand_backend/post/urls.py
@@ -2,12 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #ex: /post/all
+    # GET ex: /post/all
     path("allItems/view", views.GetAllItems, name="GetAllItems"),
-    #ex: /post/singleItem/view/asd341213sdwqa
+
+    # GET ex: /post/singleItem/view/asd341213sdwqa
     path("singleItem/view/<uuid:item_id>", views.GetSingleItem, name="GetSingleItem"),
-    #ex: /post/singleItem/create/aavcssd213
+
+    # POST ex: /post/singleItem/create/aavcssd213
     path("singleItem/create", views.CreateItem, name="CreateItem"),
-    #ex: /post/singleItem/update/aavcssd213
-    path("singleItem/update/<uuid:item_id>", views.UpdateItem, name="UpdateItem")
+
+    # PATCH ex: /post/singleItem/update/aavcssd213
+    path("singleItem/update/<uuid:item_id>", views.UpdateItem, name="UpdateItem"),
+
+    # DELETE ex: /post/singleItem/delete/aavcssd213
+    path("singleItem/delete/<uuid:item_id>", views.DeleteItem, name="DeleteItem")
 ]
