140c43566abc7a82fba5b5878d5ed1172b17552c
Todd Tao <jtao26@jhu.edu>
2024-02-28 14:11:25 -0500

    refactor post API, add version 1, merge same API dup functions

--- sechand_backend/post/urls.py
+++ sechand_backend/post/urls.py
@@ -3,20 +3,14 @@ from . import views
 
 urlpatterns = [
     # GET 
-    path("User/Items/all", views.GetAllUserItems, name="GetAllUserItems"),
+    path("UserItems/all", views.GetAllUserItems, name="GetAllUserItems"),
 
     # GET
     path("Items/all", views.GetAllItems, name="GetAllItems"),
 
-    # GET
-    path("Item/<uuid:item_id>", views.GetItem, name="GetSingleItem"),
+    # GET PATCH DELETE
+    path("Item/<uuid:item_id>", views.ProcessSingleItem, name="GetSingleItem"),
 
     # POST
-    path("Item/new", views.CreateItem, name="CreateItem"),
-
-    # PATCH
-    path("Item/<uuid:item_id>", views.UpdateItem, name="UpdateItem"),
-
-    # DELETE
-    path("Item/<uuid:item_id>", views.DeleteItem, name="DeleteItem")
+    path("Item/new", views.CreateItem, name="CreateItem")
 ]
--- sechand_backend/post/views.py
+++ sechand_backend/post/views.py
@@ -10,8 +10,20 @@ import uuid
 # Model(Item): id name description tags price user_id
 
 @api_view(['GET'])
+def GetAllUserItems(request):
+    # TODO: verify Seller qualification
+    user_id = request.data.get('user_id')
+    user_items = Item.objects.filter(user_id = user_id)
+    if user_items.exists():
+        serializer = ItemSerializer(user_items, many=True)
+        return JsonResponse(serializer.data, status=200)
+    else:
+        return JsonResponse({}, status=200)
+
+@api_view(['GET'])
 @permission_classes([AllowAny])
 def GetAllItems(request):
+    # get user status
     count = request.GET.get('count', 20)
     # Sanitize params
     try:
@@ -26,68 +38,54 @@ def GetAllItems(request):
     serializer = ItemSerializer(all_items, many=True)
     return JsonResponse(serializer.data, safe=False, status=200)
 
-@api_view(['GET'])
+@api_view(['GET', 'PATCH', 'DELETE'])
 @permission_classes([AllowAny])
-def GetItem(request, item_id):
-    try:
-        item = Item.objects.get(id = item_id)
-        serializer = ItemSerializer(item)
-        return JsonResponse(serializer.data, status=200)
-    except Item.DoesNotExist:
-        return JsonResponse({'error': 'Item not found'}, status=404)
-
-@api_view(['GET'])
-def GetAllUserItems(request):
-    # TODO: verify Seller qualification
-    user_id = request.data.get('user_id')
-    user_items = Item.objects.filter(user_id = user_id)
-    if user_items.exists():
-        serializer = ItemSerializer(user_items, many=True)
-        return JsonResponse(serializer.data, status=200)
-    else:
-        return JsonResponse({}, status=200)
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def CreateItem(request):
-    # TODO: verify User login status
-    req_data = request.data
-    saved = False
-    while not saved:
-        req_data['id'] = uuid.uuid4()
-        serializer = ItemSerializer(data=req_data)
+def ProcessSingleItem(request, item_id):
+    if(request.method == "GET"):
+        try:
+            item = Item.objects.get(id = item_id)
+            serializer = ItemSerializer(item)
+            return JsonResponse(serializer.data, status=200)
+        except Item.DoesNotExist:
+            return JsonResponse({'error': 'Item not found'}, status=404)          
+    elif(request.method == "PATCH"):
+        try:
+            # TODO: verify Seller qualification
+            item = Item.objects.get(id=item_id)
+        except Item.DoesNotExist:
+            return JsonResponse({'error': 'Item not found'}, status=404)
+        
+        serializer = ItemSerializer(item, data=request.data, partial=True)
         if serializer.is_valid():
-            try:
-                serializer.save()
-                saved = True
-                return JsonResponse(serializer.data, status=201)
-            except IntegrityError:
-                continue
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
         else:
             return JsonResponse(serializer.errors, status=400)
-    
-@api_view(['PATCH'])
-def UpdateItem(request, item_id):
-    try:
-        # TODO: verify Seller qualification
-        item = Item.objects.get(id=item_id)
-    except Item.DoesNotExist:
-        return JsonResponse({'error': 'Item not found'}, status=404)
-    
-    serializer = ItemSerializer(item, data=request.data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return JsonResponse(serializer.data, status=200)
-    else:
-        return JsonResponse(serializer.errors, status=400)
-    
-@api_view(['DELETE'])
+    elif(request.method == "DELETE"):
+        try:
+            # TODO: verify Seller qualification
+            item = Item.objects.get(id=item_id)
+            item.delete()
+            return JsonResponse({'message': 'Item deleted'}, status=200)
+        except Item.DoesNotExist:
+            return JsonResponse({'error': 'Item not found'}, status=404)
+        
+@api_view(['POST'])
+# TODO: remove when actural release
 @permission_classes([AllowAny])
-def DeleteItem(request, item_id):
-    try:
-        # TODO: verify Seller qualification
-        item = Item.objects.get(id=item_id)
-        item.delete()
-        return JsonResponse({'message': 'Item deleted'}, status=200)
-    except Item.DoesNotExist:
-        return JsonResponse({'error': 'Item not found'}, status=404)
+def CreateNewItem(request):
+    # TODO: verify Seller qualification
+        req_data = request.data
+        saved = False
+        while not saved:
+            req_data['id'] = uuid.uuid4()
+            serializer = ItemSerializer(data=request.data)
+            if serializer.is_valid():
+                try:
+                    serializer.save()
+                    saved = True
+                    return JsonResponse(serializer.data, status=201)
+                except IntegrityError:
+                    continue
+            else:
+                return JsonResponse(serializer.errors, status=400)
