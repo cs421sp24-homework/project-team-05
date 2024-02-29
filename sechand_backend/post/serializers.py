125c687dcce6ecfc4cef9df01e3173c3ee62b809
Todd Tao <jtao26@jhu.edu>
2024-02-28 23:02:29 -0500

    edit model, correct serializer, make migrations

--- sechand_backend/post/serializers.py
+++ sechand_backend/post/serializers.py
@@ -6,7 +6,7 @@ from .models import Item
 class SolelyItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ('id', 'name', 'description', 'tags', 'price', 'user_id', 'is_sold')
+        fields = ('id', 'name', 'description', 'catagory', 'price', 'user_id', 'is_sold')
         # Below is used for test purposes, will be REMOVED in future
         # fields = ('id', 'name', 'description', 'tags', 'price',)
 
@@ -15,7 +15,7 @@ class ItemSerializerWithSellerName(serializers.ModelSerializer):
 
     class Meta:
         model = Item
-        fields = ['id', 'name', 'description', 'tags', 'price', 'user_id', 'displayname', 'is_sold']
+        fields = ['id', 'name', 'description', 'catagory', 'price', 'user_id', 'displayname', 'is_sold']
 
     def get_displayname(self, obj):
         return obj.user_id.displayname if obj.user_id else None
