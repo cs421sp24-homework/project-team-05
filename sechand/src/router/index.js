3d05dcfc54642c97a8232fc8bc79ed422d12e69e
unknown <zxu129@jh.com>
2024-03-02 14:58:31 -0500

    front-end test

--- sechand/src/router/index.js
+++ sechand/src/router/index.js
@@ -8,6 +8,7 @@ import EditItem from '../views/EditItem.vue'
 import Login from '.././views/Login.vue'
 import Signup from '.././views/Signup.vue'
 import Profile from '.././views/Profile.vue'
+import Empty from '@/views/Empty.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,6 +37,16 @@ const router = createRouter({
       meta: { requiresAuth: true }
     },
     {
+      path: '/showitem',
+      component: Empty,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/edititem',
+      component: Empty,
+      meta: { requiresAuth: true }
+    },
+    {
       path: '/showitem/:id',
       name: 'ShowItem',
       component: ShowItem,
@@ -50,32 +61,37 @@ const router = createRouter({
     {
         path: '/login',
         component: Login,
-        name: 'login'
+        name: 'Login',
+        meta: { requiresAuth: false }
     },
     {
         path: '/signup',
         component: Signup,
-        
+        name: "Signup",
+        meta: { requiresAuth: false }
     },
     {
         path: '/profile',
         component: Profile,
+        name: 'Profile',
         meta: { requiresAuth: true }
     },
   ]
 })
 
 router.beforeEach((to, from, next) => {
-  const isAuthenticated = localStorage.getItem('access_token');
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (!isAuthenticated) {
-      next({ name: 'Home' });
-    } else {
-      next(); 
+    const isAuthenticated = localStorage.getItem('access_token');
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!isAuthenticated) {
+            next({ name: 'Home' });
+        } 
+        else {
+            next();
+        }
+    } 
+    else {
+        next(); 
     }
-  } else {
-    next(); 
-  }
 });
 
 
