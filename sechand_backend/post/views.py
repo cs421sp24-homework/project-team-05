08cab0b2c64aa1c8807b58ff6f522e8b86ff2a22
elainexia19 <elainexmz@hotmail.com>
2024-03-09 20:33:36 -0500

    fix refresh token

--- sechand_backend/post/views.py
+++ sechand_backend/post/views.py
@@ -30,13 +30,14 @@ def GetAllUserItems(request):
 @permission_classes([AllowAny])
 def GetAllItems(request):
     # get user status
-    print(request.data)
-    count = request.data.get('count',20)
+    print("get all items data", request.data)
+    count = request.data.get('count', 20)
     # Sanitize params
     try:
         count = int(count)
         print(count)
     except ValueError:
+        print("Invalid count parameter, must be an integer")
         return JsonResponse({'error': 'Invalid count parameter, must be an integer'}, status=status.HTTP_400_BAD_REQUEST)
     
     if count < 1 or count > 30:
