2b2a55bfdb49a366ccaec34a321afb2e71222a57
elainexia19 <elainexmz@hotmail.com>
2024-03-16 16:54:12 -0400

    change token time

--- sechand_backend/sechand_backend/settings.py
+++ sechand_backend/sechand_backend/settings.py
@@ -175,7 +175,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(hours=5),
 }
 
