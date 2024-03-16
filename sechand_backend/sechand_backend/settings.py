dd5acc6b91a638cf36869a171b07bce7dd838d25
elainexia19 <elainexmz@hotmail.com>
2024-03-16 04:01:48 -0400

    fix 5

--- sechand_backend/sechand_backend/settings.py
+++ sechand_backend/sechand_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "corsheaders",
     "channels",
+    "channels_redis",
     "rest_framework",
     "rest_framework_simplejwt",
     "post",
