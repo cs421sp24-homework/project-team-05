10b2cba1cd8b9f42240aa7c16f8e3f2c756f5afc
elainexia19 <elainexmz@hotmail.com>
2024-04-06 14:40:09 -0400

    fix aws

--- sechand_backend/sechand_backend/settings_test.py
+++ sechand_backend/sechand_backend/settings_test.py
@@ -3,10 +3,10 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sechandtestdb',
-        'USER': 'testuser',
-        'PASSWORD': 'testuser123456',
-        'HOST': 'testdb.cx2g2ywcaj8r.us-east-2.rds.amazonaws.com',
+        'NAME': 'oose_test',
+        'USER': 'sechandtest',
+        'PASSWORD': '12qwaszx',
+        'HOST': 'database-1.cx2g2ywcaj8r.us-east-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
